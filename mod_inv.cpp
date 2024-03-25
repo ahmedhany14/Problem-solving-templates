f3f67b728934674901b0a3907c15dceba41a44b6
Ahmed Hany <96751494+ahmedhany14@users.noreply.github.com>
2024-03-25 02:03:43 +0200

    Update mod_inv.cpp

--- mod_inv.cpp
+++ mod_inv.cpp
@@ -1,15 +1,21 @@
-int const maxn = 200005;
-ll fact[maxn] , inv_fact[maxn];
-int const inv_mod = 998244353;
-ll binpow(ll a,ll b) {
-  a %= inv_mod;
-  ll res = 1;
-  while (b > 0)(b & 1 ? res = res * a % inv_mod : res), a = a * a % inv_mod, b >>= 1;
-  return res;
+const ll N = 2e6 + 5, inv_mod = 998244353; // 1e9 + 7
+
+ll fact[N], inv_fact[N];
+
+ll mul(const ll &a, const ll &b) { return (a % inv_mod + inv_mod) * (b % inv_mod + inv_mod) % inv_mod; }
+
+ll binpow(ll a, ll b) {
+    a %= inv_mod;
+    ll res = 1;
+    while (b > 0)(b & 1 ? res = res * a % inv_mod : res), a = a * a % inv_mod, b >>= 1;
+    return res;
 }
-ll inv_p(ll a) { return binpow(a, inv_mod - 2); }
-ll C(ll n , ll k) { return fact[n] * inv_fact[n - k] % inv_mod * inv_fact[k]% inv_mod; }
-void modify() {
-  fact[0] = 1;
-  for (int i = 1; i < maxn; i++)fact[i] = fact[i - 1] * i % inv_mod, inv_fact[i] = inv_p(fact[i]);
+
+ll C(ll n, ll r) { return r > n ? 0 : mul(mul(fact[n], inv_fact[n - r]), inv_fact[r]); }
+
+void build() {
+    fact[0] = 1;
+    for (int i = 1; i < N; ++i) fact[i] = mul(fact[i - 1], i);
+    inv_fact[N - 1] = binpow(fact[N - 1], inv_mod - 2);
+    for (int i = N - 2; ~i; --i) inv_fact[i] = mul(inv_fact[i + 1], i + 1);
 }
